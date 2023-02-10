1589084c1ae61559daf94d7aae095e659b1a1903
Martin <mgwrenst@stud.ntnu.no>
2023-02-10 13:06:54 +0100

    #added support for axios

--- Frontend/vue-project/src/App.vue
+++ Frontend/vue-project/src/App.vue
@@ -1,20 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Drift from "./components/Drift.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <Drift />
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
