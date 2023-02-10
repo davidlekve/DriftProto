d6dd2f9534719e75e3f58ebc6a21e09f6518d46f
Frank William Daniels <frank.daniels@bouvet.no>
2023-02-10 13:40:08 +0100

    Added drifting object api

--- WebApiTest2/DriftingObject.cs
+++ WebApiTest2/DriftingObject.cs
@@ -2,6 +2,7 @@ namespace WebApiTest2
 {
     public class DriftingObject
     {
+        
         public String name { get; set; }
         public double latitude { get; set; }
         public double longitude { get; set; }
